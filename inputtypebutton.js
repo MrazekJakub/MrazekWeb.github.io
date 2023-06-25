35cb12dc73dfaaadb12bfcd5f1d8703374a00dd6
Jakub Mrázek <137694816+MrazekJakub@users.noreply.github.com>
2023-06-25 17:15:22 +0200

    Add files via upload

---
+++ inputtypebutton.js
@@ -0,0 +1,15 @@
+document.getElementById("aMe").addEventListener("click", function() {
+    window.location.href = "aboutme.html";
+});
+
+document.getElementById("MyP").addEventListener("click", function() {
+    window.location.href = "myprojects.html";
+});
+
+document.getElementById("Cer").addEventListener("click", function() {
+    window.location.href = "certificates.html";
+});
+
+document.getElementById("Con").addEventListener("click", function() {
+    window.location.href = "contact.html";
+});
